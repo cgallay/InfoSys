fix Softmax dim

models.py
@@ -73,6 +73,6 @@ class BaselineCNN(nn.Module):
         self.eval()
         with torch.no_grad():
             x = self.forward(x)
-        return nn.Softmax(dim=7)(x)
+        return nn.Softmax(dim=1)(x)
         
 

train.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
             with torch.no_grad():
                 outputs = model(inputs)
             loss = criterion(outputs, labels)
-            pred = nn.Softmax()(outputs).argmax(1)
+            pred = nn.Softmax(dim=1)(outputs).argmax(1)
             losses.update(loss.item(), labels.shape[0])
             acc = accuracy_score(labels.cpu().numpy(), pred.cpu().numpy())
             accuracy.update(acc, labels.shape[0])